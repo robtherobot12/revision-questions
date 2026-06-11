47a966a07b98af77b9ad28526e8229e860fe3d1f
robtherobot12 <robert.kvasov@gmail.com>
2026-06-11 19:30:55 +0100

    Update FsSetFrame.jsx

--- src/components/FsSetFrame.jsx
+++ src/components/FsSetFrame.jsx
@@ -1,31 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Flashcard from './Flashcard';
 
 export default function FsSetFrame({ dataset }) {
 
     const [card, setCard] = useState(null)
     const [usedNumbers, setUsedNumbers] = useState([])
+    const [cardNums, setCardNums] = useState([])
+
+    useEffect(() => {
+        let nums = []
+      for (let i = 0; i < dataset.length; i++) {
+        nums.push(i);
+      }
+      setCardNums(nums)
+    }, [dataset])
+    
 
     function getPreviousCard() {
-        
+
     }
 
     function getRandomCard() {
-        let availableUsedNumbers = usedNumbers;
+        // let availableUsedNumbers = usedNumbers;
 
-        if (availableUsedNumbers.length >= dataset.length - 1) {
-            availableUsedNumbers = [];
-        }
+        // if (availableUsedNumbers.length >= dataset.length - 1) {
+        //     availableUsedNumbers = [];
+        // }
 
-        let randomNum;
-        do {
-            randomNum =
-                Math.floor(Math.random() * dataset.length);
-        }
-        while (usedNumbers.includes(randomNum));
+        // let randomNum;
+        // do {
+        //     randomNum =
+        //         Math.floor(Math.random() * dataset.length);
+        // }
+        // while (usedNumbers.includes(randomNum));
 
-        setUsedNumbers([...availableUsedNumbers, randomNum]);
-        setCard(dataset[randomNum])
+        let randomNum = Math.floor(Math.random() * cardNums.length);
+        let cardNum = cardNums[randomNum]
+
+        console.log(cardNums)
+        console.log(randomNum)
+        console.log(cardNum)
+        setCardNums(cardNums.splice(randomNum, 1))
+        console.log(cardNums)
+        setUsedNumbers([...usedNumbers, cardNum]);
+        setCard(dataset[cardNum])
     }
 
     return (
